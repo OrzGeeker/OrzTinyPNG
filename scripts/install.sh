a497e2655f30d5763311ebf190cf719035418eab
wangzhizhou <824219521@qq.com>
2022-03-20 23:14:03 +0800

    add install script and readme.md

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#-*- coding: utf-8 -*-
+
+# Install Homebrew for MacOS or Linux
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+# install tinyPNG with homebrew
+brew install tinyPNG
