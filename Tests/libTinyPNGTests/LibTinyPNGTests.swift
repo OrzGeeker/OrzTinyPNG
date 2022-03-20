8ad952300b25d4637ba58e70361737676cef5cbb
wangzhizhou <824219521@qq.com>
2022-03-20 12:05:48 +0800

    add tests

--- Tests/libTinyPNGTests/LibTinyPNGTests.swift
+++ Tests/libTinyPNGTests/LibTinyPNGTests.swift
@@ -8,45 +8,57 @@
 import XCTest
 @testable import libTinyPNG
 import Foundation
+import Foundation.NSBundle
 
 class LibTinyPNGTests: XCTestCase {
+    /// [testTarget 中获取Resources](https://developer.apple.com/documentation/swift_packages/bundling_resources_with_a_swift_package)
+    let testResourceBundle = Bundle.module
     let client = TinyPNGAPI(apiKey: "MWmX23aKKpW6wsJjVC3gW1YUhh6CDOID")
     let testImageURL = URL(string: "https://tinify.com/static/images/globe-map.png")
     let testBundle = Bundle(for: LibTinyPNGTests.self)
-    lazy var testTempDirFileURL: URL = {
-        return URL(string: "file://\(NSHomeDirectory())")!.appendingPathComponent("Desktop") .appendingPathComponent(testBundle.bundleIdentifier!)
+    lazy var testTempDirFileURL: URL? = {
+        if let bundleId = testBundle.bundleIdentifier, let desktopDirFileURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+            let tempDirURL = desktopDirFileURL.appendingPathComponent(bundleId)
+            return tempDirURL;
+        }
+        return nil
     }()
     func testCompressImageWithURLAndDownloadResultAndResizeIt() async throws {
         let (error, result) = try await client.compress(url: testImageURL)
-        XCTAssertNil(error)
+        XCTAssertNil(error, "网络图片压缩失败")
         XCTAssertNotNil(result)
         if let ret = result {
             let compressedImageData = try await client.download(url: ret.output.url)
-            XCTAssertNotNil(compressedImageData)
+            XCTAssertNotNil(compressedImageData, "压缩后的图片下载失败")
             let resizeModel = TinyPNGAPIRequestModel.Resize(.fit, width: ret.output.width / 2 , height: ret.output.height / 2)
             let resizedImageData = try await client.resize(for: ret.output.url, resizeModel: resizeModel)
-            XCTAssertNotNil(resizedImageData)
+            XCTAssertNotNil(resizedImageData, "压缩后的图片缩放失败")
         }
     }
     func testCompressImageWithData() async throws {
         let imageData = try Data(contentsOf: testImageURL!)
         let (error, result) = try await client.compress(imageData: imageData)
-        XCTAssertNil(error)
+        XCTAssertNil(error, "本地图片压缩失败")
         XCTAssertNotNil(result)
     }
     func testCompressLocalImageFile() async throws {
-        // TODO: 获取Bundle中的图片资源文件URL
-        if let srcFileURL = URL(string: "file:///Users/joker/Downloads/test.webp") {
-            let dstFileURL = testTempDirFileURL.appendingPathComponent(srcFileURL.lastPathComponent)
-            let ret = await client.compressImage(for: srcFileURL, dstFileURL: dstFileURL)
-            XCTAssertTrue(ret)
+        XCTAssertNotNil(testTempDirFileURL, "获测试用的临时目录失败")
+        let testImageFileURLs = ["png", "jpg", "JPEG", "webp"]
+            .map { testResourceBundle.url(forResource: "test", withExtension: $0) }
+            .compactMap { $0 }
+        for testImageFileURL in testImageFileURLs {
+            XCTAssertNotNil(testImageFileURL, "测试图片路径获取失败")
+            if let testTempDirFileURL = testTempDirFileURL {
+                let dstFileURL = testTempDirFileURL.appendingPathComponent(testImageFileURL.lastPathComponent)
+                let ret = await client.compressImage(for: testImageFileURL, dstFileURL: dstFileURL)
+                XCTAssertTrue(ret, "压缩图片失败")
+            }
         }
     }
-    
     override func tearDownWithError() throws {
         // 测试结束后，清理临时文件目录
-        if FileManager.default.fileExists(atPath: testTempDirFileURL.path) {
-            try FileManager.default.removeItem(at: testTempDirFileURL)
+        if let testTempDirFileURL = testTempDirFileURL, FileManager.default.fileExists(atPath: testTempDirFileURL.path) {
+            try FileManager.default.removeItem(atPath: testTempDirFileURL.path)
         }
     }
 }
