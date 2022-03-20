1e7c520f1a30a15c684c2479be73229b3c658d0c
wangzhizhou <824219521@qq.com>
2022-03-21 00:52:35 +0800

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 # Install Homebrew for MacOS or Linux
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install tinyPNG with homebrew
 brew tap orzgeeker/orzgeeker
